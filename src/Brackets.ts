76fd41c4a10accc8c78cc58201c1a6e94df9c5e5
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-15 04:12:23 +0000

    15 Jun 2023, 09:42

--- src/Brackets.ts
+++ src/Brackets.ts
@@ -13,11 +13,24 @@ export default class Brackets implements IBracketValidator{
         }
 
 
-        const set= new Set()
+        const arr = []
         
         for(let char of str ){
 
-            if(char==='')
+            if(char==='['){
+              arr.push(char)
+            }
+            else{
+              arr.pop()
+            }
+          
+        }
+       
+        if(arr.length===0){
+          result="OK"
+        }
+        else{
+           result ="FAIL"
         }
     
         return result
